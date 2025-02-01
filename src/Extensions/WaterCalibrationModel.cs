0a41eca52cc714a894e15be2503ad60727f49d01
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-01-31 17:21:54 -0800

    Log calibration to file

--- src/Extensions/WaterCalibrationModel.cs
+++ src/Extensions/WaterCalibrationModel.cs
@@ -4,22 +4,39 @@ using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using MathNet.Numerics;
+using AindBehaviorWaterTuner.DataTypes;
+using System.Collections.Generic;
 
 [Combinator]
-[Description("")]
+[Description("Computes a water calibration model from a sequence of measurements.")]
 [WorkflowElementCategory(ElementCategory.Transform)]
 public class WaterCalibrationModel
 {
-    public IObservable<LinearRegressionFit> Process(IObservable<Tuple<double, ScaleMessage, ScaleMessage>[]> source)
+    public IObservable<WaterValveCalibration> Process(IObservable<IEnumerable<Measurement>> source)
     {
         return source.Select(value => {
-            var delays = value.Select(x => x.Item1).ToArray();
-            var weights = value.Select(x => x.Item3.DataFrame.Weight - x.Item2.DataFrame.Weight).ToArray();
-            Tuple<double, double> p = Fit.Line(delays, weights);
-            return new LinearRegressionFit(){
-                Intercept = p.Item1,
+
+            var input = new WaterValveCalibrationInput(){
+                Measurements = value.ToList()
+            };
+
+
+            var delays = value.Select(x => x.ValveOpenTime).ToArray();
+            var weights = value.Select(x => x.WaterWeight.Average()).ToArray();
+            var p = Fit.Line(delays, weights);
+
+            var output = new WaterValveCalibrationOutput(){
+                Offset = p.Item1,
                 Slope = p.Item2,
-                R2 = GoodnessOfFit.RSquared(delays.Select(x => p.Item1+p.Item2*x), weights)
+                R2 = GoodnessOfFit.RSquared(delays.Select(x => p.Item1+p.Item2*x), weights),
+                IntervalAverage = value.ToDictionary(k => k.ValveOpenTime.ToString("0.0000"), v => v.WaterWeight.Average()),
+                ValidDomain = delays.ToList()
+            };
+
+            return new WaterValveCalibration(){
+                Input = input,
+                Output = output,
+                Date = DateTimeOffset.Now,
             };
         });
     }
