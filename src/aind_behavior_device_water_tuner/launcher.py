db226348c42e8d6ac0a4ab0d4095a8f6ee0618d0
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-02-03 16:57:07 -0800

    Fix Launcher imports and add storage constraint

--- src/aind_behavior_device_water_tuner/launcher.py
+++ src/aind_behavior_device_water_tuner/launcher.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 import aind_behavior_experiment_launcher.launcher.behavior_launcher as behavior_launcher
-from aind_behavior_experiment_launcher.apps.app_service import BonsaiApp
+from aind_behavior_experiment_launcher import resource_monitor
+from aind_behavior_experiment_launcher.apps import BonsaiApp
 from aind_behavior_services.session import AindBehaviorSessionModel
 
 from aind_behavior_device_water_tuner.rig import AindBehaviorWaterTunerRig
@@ -12,6 +13,13 @@ def make_launcher() -> behavior_launcher.BehaviorLauncher:
     data_dir = r"C:/Data"
     srv = behavior_launcher.BehaviorServicesFactoryManager()
     srv.attach_bonsai_app(BonsaiApp(Path(r"./src/main.bonsai")))
+    srv.attach_resource_monitor(
+        resource_monitor.ResourceMonitor(
+            constrains=[
+                resource_monitor.available_storage_constraint_factory(data_dir, 2e11),
+            ]
+        )
+    )
 
     return behavior_launcher.BehaviorLauncher(
         rig_schema_model=AindBehaviorWaterTunerRig,
